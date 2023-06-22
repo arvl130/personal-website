027a0d8b2a6535366de09c4174887874e90dd637
Angelo Geulin <angelogeulin123@gmail.com>
2023-06-23 06:14:53 +0800

    feat: remove commas in tools list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -99,8 +99,8 @@ import informationSystemImgPath from "../assets/imgs/projects/information-system
         Tools I'm Familiar With
       </h2>
       <p class="text-xl text-white text-center">
-        React, Next.js, Vue, Nuxt.js, Node.js, Express, tRPC, TailwindCSS,
-        Firestore, PHP, MySQL, HTML, CSS, JavaScript
+        React Next.js Vue Nuxt.js Node.js Express tRPC TailwindCSS Firestore PHP
+        MySQL HTML CSS JavaScript
       </p>
     </div>
   </section>
