a061dc2fafe5ca1f4123228b63c52e258a1af40c
Angelo Geulin <angelogeulin123@gmail.com>
2025-01-18 23:53:43 +0800

    chore: update copyright year

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="text-center px-3 py-4 bg-neutral-800 text-white dark:bg-white dark:text-neutral-800">
-      <p className="font-semibold">Angelo Geulin &copy; 2024</p>
+      <p className="font-semibold">Angelo Geulin &copy; 2025</p>
       <p className="text-xs italic font-light mt-1">
         Software should be free and should empower its users.
       </p>
